f6cc7518bb3f4bcb7f9ef8f9a98c274121ce13c3
vidya <vsagarj@gmail.com>
2020-06-16 21:07:16 -0700

    Updates

---
+++ precache-manifest.6565742f8d0f7650f213e65eee87f7e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5d3b289f41d2741a9548f067384d08",
+    "url": "/miles-rewards/index.html"
+  },
+  {
+    "revision": "ade6355f3f9913b56a38",
+    "url": "/miles-rewards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f3d708c7c821d4d853f",
+    "url": "/miles-rewards/static/js/2.74be9b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miles-rewards/static/js/2.74be9b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade6355f3f9913b56a38",
+    "url": "/miles-rewards/static/js/main.2fd53549.chunk.js"
+  },
+  {
+    "revision": "4d32bf2326acee4996ba",
+    "url": "/miles-rewards/static/js/runtime-main.2ab8ca3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miles-rewards/static/media/logo.5d5d9eef.svg"
+  }
+]);
